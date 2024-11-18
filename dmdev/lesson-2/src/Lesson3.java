9c4388f93c5d86ec0d2ad8fe72070a29478f6d96
Reunkedze <reunok2000@gmail.com>
2024-11-19 01:21:48 +0300

    first commit

---
+++ dmdev/lesson-2/src/Lesson3.java
@@ -0,0 +1,8 @@
+public class Lesson3 {
+    public static void main(String[] args) {
+        int value = 5;
+        int value2 = 3;
+        double sum = value / (double) value2;
+        System.out.println(sum);
+    }
+}
---
+++ dmdev/lesson-4/src/com/reunkedze/lesson4/SwitchExample.java
@@ -0,0 +1,18 @@
+package com.reunkedze.lesson4;
+
+public class SwitchExample {
+
+    public static void main(String[] args) {
+        int value = 3;
+        switch (value) {
+            case 1:
+                System.out.println("equal 1");
+                break;
+            case 2:
+                System.out.println("equal 2");
+                break;
+            default:
+                System.out.println("no value");
+        }
+    }
+}
